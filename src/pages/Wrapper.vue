61f366395e18b58ff05dc489474e27ba961ee77f
jrixgeromo <jrixgeromo@gmail.com>
2021-08-30 21:10:34 +0800

    now logic updates

--- src/pages/Wrapper.vue
+++ src/pages/Wrapper.vue
@@ -1,54 +1,54 @@
 <template>
-  <vue-element-loading :active="isActive" :is-full-screen="true"/>
-  <Nav/>
+  <!-- <vue-element-loading :active="isActive" :is-full-screen="true" /> -->
+  <Nav />
   <section class="dashboard-main-sec">
     <div class="dash-content-main">
       <div class="sidebar-main-inner">
-        <Menu/>
+        <Menu />
       </div>
       <div class="dash-content-area">
-        <router-view/>
+        <router-view />
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Nav from '@/components/Nav';
-import Menu from '@/components/Menu';
-import axios from "axios";
-import {ref, onMounted} from 'vue';
-import {useRouter} from "vue-router";
-import {useStore} from "vuex";
-import VueElementLoading from "vue-element-loading";
+import Nav from '@/components/Nav'
+import Menu from '@/components/Menu'
+import axios from 'axios'
+import { /* ref, */ onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+// import VueElementLoading from 'vue-element-loading'
 
 export default {
-  name: "Wrapper",
+  name: 'Wrapper',
   components: {
     Nav,
     Menu,
-    VueElementLoading
+    // VueElementLoading,
   },
   setup() {
-    const router = useRouter();
-    const store = useStore();
-    const isActive = ref([]);
+    const router = useRouter()
+    const store = useStore()
+    // const isActive = ref([])
 
     onMounted(async () => {
       try {
-        isActive.value = true;
-        const {data} = await axios.get('user');
-        await store.dispatch('User/setUser', data);
-        isActive.value = false;
+        // isActive.value = true
+        const { data } = await axios.get('user')
+        // isActive.value = false
+        await store.dispatch('User/setUser', data)
       } catch (e) {
-        isActive.value = false;
-        await router.push('/login');
+        // isActive.value = false
+        await router.push('/login')
       }
     })
 
     return {
-      isActive
+      // isActive,
     }
-  }
+  },
 }
 </script>
